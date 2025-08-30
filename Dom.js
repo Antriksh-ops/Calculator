2d9d08adeb0cae557fa6e4bd699a3f2467a60c9e
Vishu <antriksh058@gmail.com>
2025-08-30 10:39:25 +0530

    started Dom

---
+++ Dom.js
@@ -0,0 +1,8 @@
+let title = document.getElementById("title")
+console.log(title)
+
+let message = document.querySelector(".message")
+console.log(message)
+
+let button = document.querySelector("button")
+console.log(button)
